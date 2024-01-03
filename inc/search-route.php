8e6154d67f05f8aa6629f529be6155afedc234cc
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-01-03 14:07:42 -0500

    Add files via upload

--- inc/search-route.php
+++ inc/search-route.php
@@ -1,8 +1,8 @@
 <?php
 
-add_action('rest_api_init', 'universityRegisterSearch');
+add_action('rest_api_init', 'marketplaceRegisterSearch');
 
-function universityRegisterSearch() {
+function marketplaceRegisterSearch() {
     register_rest_route('ebookMarketplace/v1', 'search', array(
         'methods' => WP_REST_SERVER::READABLE,
         'callback' => 'marketplaceSearchResults'
@@ -62,57 +62,39 @@ function marketplaceSearchResults($data) {
         }
     }
 
-    $shortQuery = new WP_Query(array(
-        'post_type' => 'short',
-        's' => sanitize_text_field($data['term'])
-    ));
+    // $shortQuery = new WP_Query(array(
+    //     'post_type' => 'short',
+    //     's' => sanitize_text_field($data['term'])
+    // ));
 
-    if ($shortQuery->posts) {
-        foreach($shortQuery->posts as $key => $queryItem) {
-            $authors = '';
-            $postId = url_to_postid( get_the_permalink($queryItem));
-            $authorsField = get_field('short_author', $postId);
-            if ($authorsField) {
-                $fieldCounter = 0;
-                while ($fieldCounter <= count($authorsField)) {
-                    $authors .= $authorsField[$fieldCounter]->post_title;
-                    if (count($authorsField) > 1 && $fieldCounter < count($authorsField) -1) {
-                        $authors .= ', ';
-                    }
-                    $fieldCounter = $fieldCounter + 1;
-                }
-            } else {
-                $authors = 'Unknown or Anonymous Author';
-            }
+    // if ($shortQuery->posts) {
+    //     foreach($shortQuery->posts as $key => $queryItem) {
+    //         $authors = '';
+    //         $postId = url_to_postid( get_the_permalink($queryItem));
+    //         $authorsField = get_field('short_author', $postId);
+    //         if ($authorsField) {
+    //             $fieldCounter = 0;
+    //             while ($fieldCounter <= count($authorsField)) {
+    //                 $authors .= $authorsField[$fieldCounter]->post_title;
+    //                 if (count($authorsField) > 1 && $fieldCounter < count($authorsField) -1) {
+    //                     $authors .= ', ';
+    //                 }
+    //                 $fieldCounter = $fieldCounter + 1;
+    //             }
+    //         } else {
+    //             $authors = 'Unknown or Anonymous Author';
+    //         }
             
-            array_push($results, array(
-                'posttype' => 'short',
-                'title' => get_the_title($queryItem),
-                'permalink' => get_the_permalink($queryItem),
-                'thumbnail' => get_the_post_thumbnail_url($queryItem),
-                'shortauthor' => $authors,
-                'excerpt' => get_the_excerpt($postId)
-            ));
-        }
-    }
-
-    $shelfQuery = new WP_Query(array(
-        'post_type' => 'curations',
-        's' => sanitize_text_field($data['term'])
-    ));
-
-    if ($shelfQuery->posts) {
-        foreach($shelfQuery->posts as $key => $queryItem) {
-            array_push($results, array(
-                'posttype' => 'curations',
-                'title' => get_the_title($queryItem),
-                'permalink' => get_the_permalink($queryItem),
-                // 'curator' => get_the_author($queryItem),
-                'thumbnail' => get_the_post_thumbnail_url($queryItem)//,
-                // 'excerpt' => the_content($queryItem)
-            ));
-        }
-    }
+    //         array_push($results, array(
+    //             'posttype' => 'short',
+    //             'title' => get_the_title($queryItem),
+    //             'permalink' => get_the_permalink($queryItem),
+    //             'thumbnail' => get_the_post_thumbnail_url($queryItem),
+    //             'shortauthor' => $authors,
+    //             'excerpt' => get_the_excerpt($postId)
+    //         ));
+    //     }
+    // }
 
     return $results;
 }
