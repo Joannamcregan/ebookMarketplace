4d58cd39142e74eb17c197b1936b015784eca7f5
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-01-17 18:17:19 -0500

    Add files via upload

--- woocommerce/myaccount/dashboard.php
+++ woocommerce/myaccount/dashboard.php
@@ -61,7 +61,7 @@ $allowed_html = array(
 <?php $user = wp_get_current_user();
 if (in_array( 'administrator', (array) $user->roles )) {
     ?><p>In the <a href="<?php echo esc_url(site_url('/wp-admin'));?>">backend</a> you can manage the site, curate bookshelves, post or update events, offers, or needs.</p><?php
-} else if (in_array( 'vendor', (array) $user->roles ) ) {
+} else if (in_array( 'dc_vendor', (array) $user->roles ) ) {
     ?><p>In the <a href="<?php echo esc_url(site_url('/wp-admin'));?>">backend</a> you can manage products, curate bookshelves, post or update events, offers, or needs.</p><?php
 } else if (is_user_logged_in()) {
 	?><p>In the <a href="<?php echo esc_url(site_url('/wp-admin'));?>">backend</a> you can curate bookshelves, post or update events, offers, or needs.</p><?php
