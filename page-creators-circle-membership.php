e6d0ea725f431ef74aa48901c404d37c542ccefa
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-06-19 16:43:18 -0400

    Add files via upload

---
+++ page-creators-circle-membership.php
@@ -0,0 +1,29 @@
+<?php get_header();
+
+?><main>
+    <div class="banner"><h1 class="centered-text">Creator Membership</h1></div>
+    <br>
+    <br>
+    <div class="generic-wrapper-2">
+        <div class="generic-wrapper-1">
+            <div class="generic-wrapper-0">
+                <h2 class="centered-text">About Creators' Circle Membership</h2>
+                <p class="centered-text">Members in the Creators' Circle enjoy the following perks:</p>
+                <ul>
+                    <li><strong>90% Royalties</strong></li>
+                    <li><strong>15% off purchases</strong></li>
+                    <li>Access to pre-pub readers, editors, book designers</li>
+                    <li>Free or reduced cost events</li>
+                    <li>Participation in offers and needs market</li>
+                    <li>Ability to create/facilitate events, such as book clubs/talks</li>
+                    <li>Profit-sharing based on participation</li>
+                    <li>Voting rights – after 3 months of membership</li>
+                </ul>
+                <p class="centered-text">Ready to join? It's as easy as completing <a href="<?php echo esc_url(site_url('/creator-membership-agreement'));?>">this form</a> and paying your first months' dues.</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer();
+?>
