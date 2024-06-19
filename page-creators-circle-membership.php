1daea7f15115e6c1fda6e9c43c7b82f96b50ce11
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-06-19 16:51:31 -0400

    Add files via upload

--- page-creators-circle-membership.php
+++ page-creators-circle-membership.php
@@ -1,29 +1,18 @@
 <?php get_header();
 
 ?><main>
-    <div class="banner"><h1 class="centered-text">Creator Membership</h1></div>
+    <div class="banner"><h1 class="centered-text">Creators' Circle</h1></div>
     <br>
     <br>
-    <div class="generic-wrapper-2">
-        <div class="generic-wrapper-1">
-            <div class="generic-wrapper-0">
-                <h2 class="centered-text">About Creators' Circle Membership</h2>
-                <p class="centered-text">Members in the Creators' Circle enjoy the following perks:</p>
-                <ul>
-                    <li><strong>90% Royalties</strong></li>
-                    <li><strong>15% off purchases</strong></li>
-                    <li>Access to pre-pub readers, editors, book designers</li>
-                    <li>Free or reduced cost events</li>
-                    <li>Participation in offers and needs market</li>
-                    <li>Ability to create/facilitate events, such as book clubs/talks</li>
-                    <li>Profit-sharing based on participation</li>
-                    <li>Voting rights – after 3 months of membership</li>
-                </ul>
-                <p class="centered-text">Ready to join? It's as easy as completing <a href="<?php echo esc_url(site_url('/creator-membership-agreement'));?>">this form</a> and paying your first months' dues.</p>
-            </div>
-        </div>
+    <?php if (is_user_logged_in()){
+        ?><div class="generic-content">
+            <h2 class="centered-text">Membership Agreement</h2>
+        <?php echo do_shortcode('[forminator_form id="4212"]'); ?>
     </div>
-</main>
+    <?php } else {
+        ?><p>Interested in becoming a Creator-Member? First, register for our community or <a href="<?php echo esc_url(site_url('/my-account'));?>">login</a>.</p>
+    <?php }
+?></main>
 
 <?php get_footer();
 ?>
