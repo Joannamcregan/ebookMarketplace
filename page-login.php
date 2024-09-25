bd0b1a00383ebb06b90d8ea9e028a4cb98817a44
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-09-25 18:31:17 -0400

    Delete page-login.php

--- page-login.php
+++
@@ -1,9 +0,0 @@
-<?php get_header();
-?><main>
-    <div class="two-thirds-screen">
-        <div class="banner"><h1 class="centered-text">404</h1></div>
-        <h1 class="center-left-text">Sorry, looks like we took a wrong turn somewhere.</h1>
-    </div>
-</main>
-
-<?php get_footer(); ?>
