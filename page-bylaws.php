b09ad37be27b3f9d91d8ac5106c928ed856991e1
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-10-16 16:43:12 -0400

    Add files via upload

--- page-bylaws.php
+++ page-bylaws.php
@@ -1,12 +1,77 @@
 <?php get_header();
 
 ?><main>
-    <div class="banner"><h1 class="centered-text banner-heading-46">Our Bylaws</h1></div>
-    <br>
     <div class="generic-content full-screen">
-        <div class="white-background-wrapper">
-            <?php wp_reset_postdata();
-            the_content(); ?>
+        <img src="<?php echo get_theme_file_uri('/images/corner-stars-0.jpg'); ?>" role="presentation" class="margin-auto-block" />
+        <h1 class="centered-text banner-heading-22">TRUNK OF MY CAR COOPERATIVE<br><img src="<?php echo get_theme_file_uri('/images/corner-stars-2.jpg'); ?>" role="presentation" class="inline" /> BYLAWS <img src="<?php echo get_theme_file_uri('/images/corner-stars-1.jpg'); ?>" role="presentation" class="inline" /></h1>
+        <h2 class="centered-text banner-heading-18">ARTICLE I: PURPOSE</h2>
+        <div class="left-gentle-bracket-orange">
+            <p>The purpose of the Trunk of My Car Cooperative (the “Cooperative”) is to nurture and embody
+            collective organizational values within a community of writers and readers and to redistribute
+            monetary resources earned via publishing to the creators and supporters of such works. 
+            </p>
+        </div>
+        <h2 class="centered-text banner-heading-18">ARTICLE II: MISSION</h2>
+        <div class="left-gentle-bracket-purple">
+            <p>Our mission is to cooperatively redirect publishing resources from those who take to those who
+            create. 
+            </p>
+        </div>
+        <h2 class="centered-text banner-heading-18">ARTICLE III: MEMBERSHIP</h2>
+        <div class="left-gentle-bracket-blue">
+            <p>SECTION 1. The Cooperative shall have four (4) circles1
+            of membership, with the designation,
+            qualifications, requirements, methods of acceptance and incidents of each circle of membership as
+            set forth below. The Cooperative shall operate on a one (1) person, one (1) vote basis and no
+            member shall possess greater ownership or control than any other member.  
+            </p>
+            <p>SECTION 2. WORKER MEMBERS: Any wage or contract employee of the Cooperative shall be
+            eligible for membership as a “Worker Member” for as long as that person is employed by the
+            Cooperative and after meeting the minimum employment eligibility requirement of the
+            Cooperative as determined by the Board of Directors from time to time. 
+            </p>
+            <p>SECTION 3. CREATOR MEMBERS: Any person shall be eligible for membership as a “Creator
+            Member” provided that that person is actively selling products through the Cooperative’s platform
+            and after complying with all equity requirements of the Cooperative as determined by the Board
+            of Directors from time to time.</p>
+            <p>SECTION 4. CONSUMER MEMBERS: Any person or entity shall be eligible for membership as
+            a “Consumer Member” provided that that person is actively purchasing products from the
+            Cooperative and after complying with all equity requirements of the Cooperative as determined
+            by the Board of Directors from time to time. 
+            </p>
+            <p>SECTION 5. INVESTOR MEMBERS: Any person or entity shall be eligible for membership as
+            an “Investor Member” provided that that person is an investor in the Cooperative and after
+            complying with all legal and equity requirements of the Cooperative as determined by the Board
+            of Directors from time to time. </p>
+            <p>SECTION 6. FORFEITURE OF MEMBERSHIP: Any member shall forfeit membership in the
+            event that the member fails to meet the requirements of membership, as determined by the Board
+            of Directors after notice to the member and an opportunity to rebut any allegations before the
+            Board. 
+            </p>
+            <p>
+            SECTION 7. TERMINATION OF MEMBERSHIP
+            </p>
+            <ol type="A">
+                <li>RESIGNATION. Members wishing to leave the Cooperative must provide notice to the
+                Cooperative through the Cooperative’s platform. Resigning members are responsible for
+                fulfilling any/all outstanding obligations. 
+                </li>
+                <li>INVOLUNTARY TERMINATION. The Board of Directors may terminate an individual
+                or organization’s membership for one of the following reasons: 
+                    <ol type="1">
+                        <li>Failing to participate in the Cooperative for one year or more in accordance
+                        with the rules and regulations associated with such membership circle.
+                        </li>
+                        <li>Violating terms of Cooperative policy, Board policy, community standards, or
+                        these Bylaws. 
+                        </li>
+                        <li>Acting contrary to the best interests of the Cooperative. Members may be
+                        expelled for cause from the Cooperative by a majority vote of the Board of
+                        Directors, in accordance with the Cooperative’s accountability policy. 
+                        </li>
+                    </o>
+                </li>
+            </ol>
         </div>
       </div>
 </main>
