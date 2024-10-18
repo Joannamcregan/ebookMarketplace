a6e2963f5629aca28d35626a8672658d6c21b73b
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-10-18 14:23:00 -0400

    Add files via upload

--- page-bylaws.php
+++ page-bylaws.php
@@ -60,18 +60,15 @@
                 or organization’s membership for one of the following reasons: 
                     <ol type="1">
                         <li>Failing to participate in the Cooperative for one year or more in accordance
-                        with the rules and regulations associated with such membership circle.
-                        </li>
+                        with the rules and regulations associated with such membership circle.</li>
                         <li>Violating terms of Cooperative policy, Board policy, community standards, or
-                        these Bylaws. 
-                        </li>
+                        these Bylaws. </li>
                         <li>Acting contrary to the best interests of the Cooperative. Members may be
                         expelled for cause from the Cooperative by a majority vote of the Board of
-                        Directors, in accordance with the Cooperative’s accountability policy. 
-                        </li>
-                    </o>
+                        Directors, in accordance with the Cooperative’s accountability policy. </li>
+                    </ol>
                 </li>
-                <li>C. Termination of membership shall proceed as follows: 
+                <li>Termination of membership shall proceed as follows: 
                     <ol type="1">
                         <li>The Board will send notice to the email address on file for such member stating
                         the reason for the proposed termination and outline specific incidences of
@@ -141,7 +138,85 @@
             acceptance of members on the basis of race, gender, religion, income, marital status, or nationality. 
             </p>
         </div>
-      </div>
+        <h2 class="centered-text banner-heading-18">ARTICLE IV. MEMBERSHIP MEETINGS</h2>
+        <div class="left-gentle-bracket-yellow">
+            <p>SECTION 1. The annual meeting shall be held no later than the last day of the fourth (4th) month after the close of the fiscal year and shall be conducted virtually. It will be the responsibility of the Board of Directors to set the exact date of the meeting. The annual meeting shall be for the purpose of electing Directors and receiving the annual audit, and any other business that may come before the members of the Cooperative.</p>
+            <p>SECTION 2. Notice of the annual meetings shall be the responsibility of the Board of Directors, who shall, prior to at least thirty (30) days to the date, provide notice to members electronically at the email address on file with the Cooperative and prominently advertise such meetings on the Cooperative’s platform. It is each member’s responsibility to ensure that the information used to contact said member is complete and up to date. Notices shall specify the purpose of the meeting.</p>
+            <p>SECTION 3. Special meetings of the members of the Cooperative may be demanded in writing at any time by order of a majority of the Board of Directors or five percent (5%) or two hundred fifty (250) members, whichever is less, and shall be called by the Secretary within thirty (30) days of said demand. The request shall state the time, place, and objective of the meeting. No business shall be transacted at Special Meetings other than what is described in the posted notices.</p>
+            <p>SECTION 4. The rules of order at membership meetings shall be the consensus decision-making process found at <a href="https://effectivecollective.net/library/consensus-in-sharing-law.pdf" target="_blank">https://effectivecollective.net/library/consensus-in-sharing-law.pdf</a>. Members are encouraged to review said rules of order prior to attending any meeting, special, annual, or otherwise.</p>
+            <p>SECTION 5. QUORUM. At any annual, special or other membership meeting, a quorum to qualify a vote shall consist of a majority of the members; except that organizational policy decisions made among Worker Members must reach consensus to take effect.</p>
+            <p>SECTION 6. Each member shall be entitled to only one (1) vote. Voting shall be conducted electronically within the Cooperation’s platform. Proxy voting shall not be allowed.</p>
+            <p>SECTION 7. Members are eligible for membership in multiple membership circles (e.g., Worker and Creator); any member belonging to multiple circles remain limited to one (1) vote as set forth in Section 6 of this Article.</p>
+        </div>
+        <h2 class="centered-text banner-heading-18">ARTICLE V. BOARD OF DIRECTORS</h2>
+        <div class="left-gentle-bracket-orange">
+            <p>SECTION 1. SELECTION.</p>
+            <ol type="A">
+                <li>The Board shall consist of five (5) Directors. </li>
+                <li>Directors must be members of the Cooperative.</li>
+                <li>There will be four (4) Directors representing each membership circle and one (1) Director voted upon at-large by all members. If in any year there are no members of a membership circle in the Cooperative, that representative seat will default to being temporarily filled at-large. The four (4) membership circle representative Directors are: 
+                    <ol type="1">
+                        <li>Worker Representative - elected by Worker Members. </li>
+                        <li>Creator Representative - elected by Creator Members.</li>
+                        <li>Consumer Representative - elected by Consumer Members.</li>
+                        <li>Investor Representative – elected by Investor Members.</li>
+                    </ol>
+                </li>
+                <li>The final at-large Director will be elected by the entire membership.</li>
+            </ol>
+            <p>SECTION 2. ELECTION. Directors will be elected by the members at annual membership meetings or at a special member meeting called to fill a vacancy. Such election shall be conducted via the Cooperatives’ platform. Directors shall be elected by a simple majority of the circle represented.</p>
+            <ol type="A">
+                <li>Directors will serve for two (2) year terms.</li>
+                <li>Terms are staggered to preserve the continuity of governance. In electing the first Board, two (2) Directors shall be elected for a one-year term, two (2) Directors for a two-year term, and one (1) Director for a three-year term, which shall be the Treasurer.</li>
+            </ol>
+            <p>SECTION 3. TERM LIMITS. No Director after having served for two (2) consecutive terms shall be eligible to succeed themselves, but after a lapse of two years shall again be eligible for election.</p>
+            <p>SECTION 4. VACANCIES. In the event of any vacancy in the Board of Directors through death, resignation, or other cause, the remaining Directors may, by a majority vote, appoint a successor to hold office until the next annual meeting, at which time a Director shall be elected to complete the terms of the Director whose place was vacant. The Board of Directors shall appoint an individual who is of the same membership circle as the vacating Director. In the event of a vacancy in an appointed position on the Board of Directors, the remaining Directors may appoint a replacement Director to fulfill the remaining term of the Director whose place was vacated.</p>
+            <p>SECTION 5. REMOVAL</p>
+            <ol type="A">
+                <li>BOARD REMOVAL
+                    <ol type="1">
+                        <li>If a Director fails to attend two (2) consecutive Board meetings without an excused absence, the Board has the option to remove that Director and fill the vacancy. </li>
+                        <li>If a Director acts contrary to the best interests of the Cooperative, the Board has the option to remove that Director and fill the vacancy.</li>
+                        <li>If the resulting vacancy is a representative of a membership circle (Worker, Creator, Consumer, or Investor), the Board will appoint another member of that same membership circle.</li>
+                    </ol>
+                </li>
+                <li>Membership Removal
+                    <ol type="1">
+                        <li>Any member of any circle may ask for removal of the Director of their circle, together with a petition signed by five percent (5%) of the members of their circle.</li>
+                        <li>An at-large Director may be petitioned for removal by any member, with a petition signed by five percent (5%) of members of any circle.</li>
+                        <li>The removal shall be voted on at the next member meeting and with two-thirds (2/3rds) vote, shall be effective.</li>
+                        <li>The Director whose removal is requested shall be served with a copy of the charges not less than ten (10) days prior to the meeting and shall have an opportunity to be heard in person and to present evidence and the persons requesting removal shall have the same opportunity. Counsel is permitted for both parties; however, the Cooperative prefers less formal mediation-style proceedings.</li>
+                    </ol>
+                </li>
+            </ol>
+            <p>SECTION 6. INDEMNIFICATION AND DIRECTOR LIABILITY. No Director of the Cooperative shall be personally liable to the Cooperative or its members for monetary damages for breach of fiduciary duty as Director, except for liability:</p>
+            <ol type="A">
+                <li>for breach of the Director’s duty of loyalty to the Cooperative or its members;</li>
+                <li>for acts or omissions not in good faith or that involve intentional misconduct or a knowing violation of law; or</li>
+                <li>for a transaction from which the Director derived an improper personal benefit.</li>
+                <li>The Cooperative shall indemnify a Director or officer in good standing for all reasonable expenses incurred in a legal proceeding due to her or his position on the Board.</li>
+            </ol>
+            <p>SECTION 7. BOARD COMPENSATION. Members of the Board of Directors shall receive no compensation for their Board service but are entitled to reasonable per diem to cover necessary expenses while engaged in the business of the Cooperative, subject to Board approval. No Board Director may vote upon reimbursement for their services.</p>
+            <p>SECTION 8. CONFLICT OF INTEREST. It shall be the duty of all Directors to complete annual conflict of interest disclosures and make prompt and full disclosure to the Board of any personal, professional, or financial conflict of interest in a matter under discussion. When a conflict of interest is disclosed, the Director must not participate in the discussion or vote on the relevant issues. No person shall be eligible to be a Director if that person is in competition with or is affiliated with any enterprise that is in competition with or is detrimental to the Cooperative. If there is a known conflict of interest, the Director must present written notice to the Board, who will have the power to vote to recognize and accept or decline the potential conflict of interest or to bring the issue to the membership for a vote to replace the said Director in conflict.</p>
+            <p>SECTION 9. BOARD MEETINGS.</p>
+            <ol type="A">
+                <li>REGULAR MEETINGS. Regular meetings of the Board of Directors shall be held at least quarterly, one immediately after the Annual Meeting of member and thereafter, at such time and date as may be fixed by the Directors.</li>
+                <li>BOARD MEETING SCHEDULE. Board meeting shall be conducted virtually. Any action required or permitted to be taken at a meeting of the Board of Directors may be taken by written action signed by all of the Directors. The written action is effective when signed by all the Directors and shall have the same force and effect as a unanimous vote at a meeting.</li>
+                <li>SPECIAL BOARD MEETINGS. Special meetings of the Board may be held from time to time. Such meetings shall be called by any two Directors and notice of at least two weeks shall be issued. Only business specified in the notice may be conducted.</li>
+                <li>OPEN BOARD MEETINGS. As a general rule, the decisions and operations of the Cooperative are open to all members. Formal notice is not required to non-Board, Cooperative members, but notice and agenda of regular and special meetings will be publicized to the membership at large. Any Cooperative member may attend meetings of the Board of Directors, as an observer. When the Board of Directors meets in Executive Session, such meetings shall be closed to non-Board of Director members.</li>
+                <li>CONSENSUS. Consensus is required for all Board decisions.</li>
+            </ol>
+            <p>SECTION 10. OFFICERS AND COMMITTEES OF THE BOARD. The Board shall elect from their number a President, Vice President, Secretary and a Treasurer. The Board may create temporary or standing committees. Committee powers shall be specifically stated by Board resolution, and in no case shall any such Committee exceed its conferred powers.</p>
+            <ol type="A">
+                <li>The principal responsibilities of the President shall be to preside at all meetings of the members and of the Board of Directors. The President shall sign all membership certificates and all contracts and other instruments, unless otherwise authorized by the Board. The President shall be an ex-officio member of all committees.</li>
+                <li>The principal responsibilities of the Vice-President shall be to discharge the duties of the President in the event of his or her absence or disability.</li>
+                <li>The principal responsibilities of the Secretary shall be to keep a true and correct record of the proceedings of all meetings of the members and of the Board of Directors; to countersign and affix the seal of the Cooperative to all papers and documents requiring such action; and to systematically see to the safety of books, papers, records, and documents pertaining to the business of the Cooperative as may be assigned to the Secretary by the Board of Directors.</li>
+                <li>The principal duties of the Treasurer shall be to sign all agreements of indebtedness and security agreements, as authorized by the Board, and to review the internal controls and financial systems used by the Cooperative when requested by the Board. The Treasurer shall perform review of all corporations or organizations to ensure that their mission and practices align with the values stated by this Cooperative. Said review shall be conducted prior to entry and at each renewal. The Treasurer shall be covered by an adequate bond as determined by the Board of Directors and at the expense of the Cooperative.</li>
+            </ol>
+            <p>SECTION 11. Within a specified period of time, any action taken by the Board of Directors must be referred to members for approval or disapproval if demanded by petition by at least five percent (5%) or two hundred fifty (250) members, whichever is less, or by majority vote of the Board of Directors, provided that rights of third parties which have vested between the time of action by the Board of Directors and approval or disapproval by the members shall not be impaired.</p>
+        </div>
+        <h2 class="centered-text banner-heading-18">ARTICLE VI FINANCE</h2>
+    </div>
 </main>
 
 <?php get_footer(); ?>
