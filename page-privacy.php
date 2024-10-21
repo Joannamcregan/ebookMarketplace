a9ece40344293ef5ddfc7a7bfdb3b0e51e20ca50
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-10-21 18:10:59 -0400

    Add files via upload

--- page-privacy.php
+++ page-privacy.php
@@ -1,12 +1,75 @@
-<?php get_header();
-
-?><main>
-    <div class="banner"><h1 class="centered-text banner-heading-46">Our Privacy Policy</h1></div>
-    <br>
-    <div class="generic-content full-screen">
-        <?php wp_reset_postdata();
-        the_content(); ?>
-      </div>
+<?php get_header(); ?>
+<main>
+  <div class="generic-content full-screen">
+    <img src="<?php echo get_theme_file_uri('/images/corner-stars-0.jpg'); ?>" role="presentation" class="margin-auto-block" />
+    <h1 class="centered-text banner-heading-22">PRIVACY NOTICE<br><img src="<?php echo get_theme_file_uri('/images/corner-stars-2.jpg'); ?>" role="presentation" class="inline" /> Last updated 4/30/2024 <img src="<?php echo get_theme_file_uri('/images/corner-stars-1.jpg'); ?>" role="presentation" class="inline" /></h1>
+    <p>Trunk of My Car Cooperative (the “Cooperative”) has compiled this Privacy Notice to describe when, where, how, what, and why we might store and/or use your information when you use the Cooperative’s platform to buy and/or sell books in various formats, offer creative and publishing assistance, participate in community forums, including, but not limited to, offers and needs markets, book clubs, workshop, and/or those related to Cooperative governance.</p>
+    <p>This notice lays out your rights, as they relate to use of the Cooperative’s online platform (the “Platform”) at www.trunkofmycar.org. Continued use of the Platform constitutes agreement with this notice. If you do not agree with any aspect of this Privacy Notice, we ask that you not use the Platform.</p>
+    <h2 class="banner-heading-18 centered-text">What We Collect</h2>
+    <div class="left-gentle-bracket-orange">
+      <p>Personal Information. We only store or use the information that you provide - this includes, but is not limited to, your account information:</p>
+      <ul>
+        <li>Your name</li>
+        <li>Email address</li>
+        <li>Password</li>
+        <li>Mailing address</li>
+        <li>User ID (system generated)</li>
+        <li>Any images or files uploaded to the Platform</li>
+        <li>Any comments you make on the Platform</li>
+      </ul>
+      <p>All information provided is anonymized and kept secure.</p>
+      <p>Sensitive Information. We do not collect, store, or use any sensitive information, except for collections of your words for sale in our bookstore.</p>
+      <p>Cookies. The Platform uses cookies and similar technologies, which are small files or pieces of text that download to a device when a visitor accesses a website or app. As your personal information is anonymized, cookies are not linked to your personal information.</p>
+      <p>Comments/ Suggestions/ Reviews. Should you leave a comment, suggestion, review, and/or posting of any kind on the Platform, we collect data shared from the posting form that includes your IP address and browser user agent string to assist with spam detection.</p>
+      <p>Login. Upon visiting our login or landing page, we will set up a temporary cookie to determine if your browser accepts cookies. This cookie contains no personal data and is discarded when you close your browser.</p>
+      <p>Upon login, we will also set up several cookies to save your login information and your screen display choices. Login cookies expire after two (2) days. Screen option cookies expire after one (1) year.</p>
+      <p>If you select "Remember Me", your login will remain open for two (2) weeks. If you log out of your account, the login cookies will be removed.</p>
+      <p>Publishing. If you edit or publish an article, create a book, upload product information, or post information about services, events, offers, or needs, an additional cookie will be saved in your browser to track and respond to your submissions. This cookie includes no personal data and simply indicates the post ID of the article you just edited; it expires after twenty-four (24) hours.</p>
+      <p>Payment. When you make a purchase or submit a payment on the Platform, we collect personal information from you to process your payment. We may collect information like your:</p>
+      <ul>
+        <li>Billing and shipping address</li>
+        <li>Details relating to your purchase</li>
+        <li>Email address</li>
+        <li>Name</li>
+        <li>Phone number</li>
+      </ul>
+      <p>Our payment processor Stripe will also collect payment information from you. You can read their privacy policy here.</p>
+    </div>
+    <h2 class="banner-heading-18 centered-text">How Long We Keep It</h2>
+    <div class="left-gentle-bracket-purple">
+      <p>We store or use the information you provide in order to make your use of the Platform easier. If you leave a comment, the comment and its metadata are retained indefinitely. This is so we can recognize and approve any follow-up comments automatically instead of holding them in a moderation queue.</p>
+      <p>For users who register on the Platform, we store the personal information they provide in their user profile.</p>
+    </div>
+    <h2 class="banner-heading-18 centered-text">How We Use Your Information/Who We Share It With</h2>
+    <div class="left-gentle-bracket-blue">
+      <p>We share applicable personal information with federal, state, and local regulatory agencies to ensure compliance to the extent required.</p>
+      <p>We may email you with messages about your order or account activity. For example, we may email you to tell you that:</p>
+      <ul>
+        <li>You’ve created a customer account</li>
+        <li>Your customer account password has been reset or updated</li>
+        <li>You’ve made a purchase</li>
+        <li>You have an outstanding invoice</li>
+        <li>Your order has shipped, if applicable</li>
+      </ul>
+      <p>It is not possible to unsubscribe from order or account activity messages.</p>
+      <p>We may send marketing emails, which you can unsubscribe from at any time by clicking the link at the bottom of the email. We share your contact information with Mailchimp, our email marketing provider, so they can send these emails on our behalf. Mailchimp’s policies can be found <a href="https://mailchimp.com/legal/" target="_blank">here</a>.</p>
+      <p>We also share your information with third-party security plug-ins to protect the Platform from cyber-attacks. We use Jetpack. Their privacy policy can be found <a href="https://jetpack.com/support/privacy/" target="_blank">here</a>.</p>
+      <p>In the event you authorize the Cooperative to disclose specified personal information to other members of the Cooperative, we will share your specified personal information with authorized members. Should you wish to revoke any previously authorized disclosure, write to us at <a href="mailto:info@trunkofmycar.org">info@trunkofmycar.org</a>. We will revoke the previously authorized disclosure within one (1) business day of your request.</p>
+      <p>We do not sell, give away, or barter your personal information for any reason to any party.</p>
+    </div>
+    <h2 class="banner-heading-18 centered-text">Where We Keep Your Information</h2>
+    <div class="left-gentle-bracket-yellow">
+      <p>On secure servers hosted by Hivelocity. Their privacy policy can be found <a href="https://www.hivelocity.net/legal/" target="_blank">here</a>.</p>
+    </div>
+    <h2 class="banner-heading-18 centered-text">Choices You Have Regarding Your Information</h2>
+    <div class="left-gentle-bracket-orange">
+      <p>All users can see, edit, or delete their personal information at any time (except you cannot change your username). If you’d like the Cooperative to remove your personal information, please write to us at info@trunkofmycar.org. We will remove your personal information within two (2) business days, and in the event you are eligible to receive a year end patronage, we will remove applicable payment details within seven (7) business days of the day we send your patronage.</p>
+    </div>
+    <h2 class="banner-heading-18 centered-text">Changes to this Policy</h2>
+    <div class="left-gentle-bracket-purple">
+      <p>We shall update this policy in accordance with all applicable laws and regulations and pursuant to the results of cooperative voting results.</p>
+    </div>
+  </div>
 </main>
 
 <?php get_footer(); ?>
