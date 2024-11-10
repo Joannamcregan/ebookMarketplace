a525e7e689ddd6db2a1ae5e43abd1317ce632074
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-11-10 18:03:26 -0500

    Add files via upload

--- page-creator-roadmap.php
+++ page-creator-roadmap.php
@@ -14,9 +14,20 @@
             <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-billing-info.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlights the Store Settings --Billing menu item." />
             <p>You can now navigate to Product Manager --Add Product in the side menu.</p>
             <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlights the Product Manager --Add Product menu item." />
-            <span class="orange-rectangle-span">How to add an e-book</span>
-            <span class="blue-rectangle-span">How to add an audiobook</span>
-            <span class="purple-rectangle-span">How to add a physical book</span>
+            <p>Add your book's title and cover.</p>
+            <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-title.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the Product Title field." />
+            <br>
+            <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-image.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the Upload Image field." />
+            <p>If you already have an ISBN, you can add it now. You can also obtain a free ISBN by purchasing our ISBN registration service after you've added your product.</p>
+            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/gtin.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the GTIN field." />
+            <p>Add your book's price. If you'd like to set a limited-time sale price, you can do that as well.</p>
+            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/price-info.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the pricing fields." />
+            <p>Be sure to indicate which book format you're uploading.</p>
+            <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-type.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the product type checkboxes." />
+            <p><strong>You'll need to complete other fields depending on the format you're uploading.</strong></p>
+            <span class="orange-rectangle-span">E-book products</span>
+            <span class="blue-rectangle-span">Audiobook products</span>
+            <span class="purple-rectangle-span">Physical book products</span>
         </div>
     </div>
     <div class="centered-90vw">
