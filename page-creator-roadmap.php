412e26bfc513ec9e008d1724ca187f8778b1e4ad
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-11-10 11:27:20 -0500

    Add files via upload

--- page-creator-roadmap.php
+++ page-creator-roadmap.php
@@ -6,10 +6,14 @@
     <span class="purple-span" id="sell-books-span">How to Sell Books</span>
     <div class="centered-90vw">
         <div class="left-gentle-bracket-orange">
-            <p>Step 1. Add product(s) in the <strong>creator dashboard</strong></p>
+            <h2>Step 1. Add product(s) in the creator dashboard</h2>
             <ul>
                 <li>If you're selling the same book in multiple formats (such as e-book, audiobook, and paperback) add a separate product for each format.</li>
             </ul>
+            <p>Before you can add any products, you must choose how you'd like to be paid.</p>
+            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-billing-info.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlights the Store Settings --Billing menu item." />
+            <p>You can now navigate to Product Manager --Add Product in the side menu.</p>
+            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlights the Product Manager --Add Product menu item." />
             <span class="orange-rectangle-span">How to add an e-book</span>
             <span class="blue-rectangle-span">How to add an audiobook</span>
             <span class="purple-rectangle-span">How to add a physical book</span>
@@ -17,7 +21,7 @@
     </div>
     <div class="centered-90vw">
         <div class="left-gentle-bracket-purple">
-            <p>Step 2. <strong>Add book info</strong> to help readers find your book</p>
+            <h2>Step 2. Add book info to help readers find your book</h2>
         </div>
     </div>
     <span class="blue-span" id="sell-products-span">How to Sell Services</span>
