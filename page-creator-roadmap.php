9f9bfebffa5f39444aaac3d390c96fc66c3627e9
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-11-11 20:32:44 -0500

    Add files via upload

--- page-creator-roadmap.php
+++ page-creator-roadmap.php
@@ -11,23 +11,43 @@
                 <li>If you're selling the same book in multiple formats (such as e-book, audiobook, and paperback) add a separate product for each format.</li>
             </ul>
             <p>Before you can add any products, you must choose how you'd like to be paid.</p>
-            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-billing-info.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlights the Store Settings --Billing menu item." />
+            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-billing-info.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlighting the Store Settings --Billing menu item." />
             <p>You can now navigate to Product Manager --Add Product in the side menu.</p>
-            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlights the Product Manager --Add Product menu item." />
+            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlighting the Product Manager --Add Product menu item." />
             <p>Add your book's title and cover.</p>
-            <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-title.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the Product Title field." />
+            <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-title.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Product Title field." />
             <br>
-            <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-image.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the Upload Image field." />
+            <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-image.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Upload Image field." />
             <p>If you already have an ISBN, you can add it now. You can also obtain a free ISBN by purchasing our ISBN registration service after you've added your product.</p>
-            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/gtin.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the GTIN field." />
+            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/gtin.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the GTIN field." />
             <p>Add your book's price. If you'd like to set a limited-time sale price, you can do that as well.</p>
-            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/price-info.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the pricing fields." />
+            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/price-info.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the pricing fields." />
             <p>Be sure to indicate which book format you're uploading.</p>
-            <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-type.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlights the product type checkboxes." />
+            <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-format.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the product format checkboxes." />
             <p><strong>You'll need to complete other fields depending on the format you're uploading.</strong></p>
             <span class="orange-rectangle-span">E-book products</span>
+            <p>Check the box next to Downloadable under Product Type.</p>
+            <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/product-type.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the downloadable product type checkbox." />
+            <p>Add your e-book file.</p>
+            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-file.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Add File button." />
+            <br>
+            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-icon.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the upload icon." />
             <span class="blue-rectangle-span">Audiobook products</span>
+            <p>Check the box next to Downloadable under Product Type.</p>
+            <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/product-type.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the downloadable product type checkbox." />
+            <p>Add your e-book file.</p>
+            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-file.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Add File button." />
+            <br>
+            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-icon.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the upload icon." />
+            <p>If you want to include a preview file, you can do so using the product gallery.</p>
+            <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-gallery.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the product gallery field." />
             <span class="purple-rectangle-span">Physical book products</span>
+            <p><strong>At this time, if you want to sell physical books, you must stock your own copies, manage your own inventory, and be responsible for shipping your own orders (including factoring US shipping into your pricing.)</strong></p>
+            <p>Check the box by 'Enable stock management at product level.'</p>
+            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/enable_stock.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the 'Enable stock management at product level' checkbox." />
+            <p>Supply values for the 'Stock quantity' and 'Low stock threshold' fields.</p>
+            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/stock.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the 'Stock quantity' and 'Low stock threshold' fields." />
+            <p>The other Inventory fields are optional.</p>
         </div>
     </div>
     <div class="centered-90vw">
@@ -36,6 +56,23 @@
         </div>
     </div>
     <span class="blue-span" id="sell-products-span">How to Sell Services</span>
+    <div class="centered-90vw">
+        <div class="left-gentle-bracket-yellow">
+            <h2>Add your service as a product in the creator dashboard</h2>
+            <p>Before you can add any products, you must choose how you'd like to be paid.</p>
+            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-billing-info.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlighting the Store Settings --Billing menu item." />
+            <p>You can now navigate to Product Manager --Add Product in the side menu.</p>
+            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlighting the Product Manager --Add Product menu item." />
+            <p>Give your service a title and main image.</p>
+            <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-title.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Product Title field." />
+            <br>
+            <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-image.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Upload Image field." />
+            <p>If you want to add additional images, you can upload them with the Product Gallery field.</p>
+            <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/product-gallery.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the product gallery field." />
+            <p>Describe your service, including any instructions that you need buyers to follow.</p>
+            <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/service-description.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the product gallery field." />
+        </div>
+    </div>
 </main>
 
 <?php get_footer();
