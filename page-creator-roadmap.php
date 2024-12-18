6ab394b54c9c2168adfa0a55aa600b5fca2840cf
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-12-18 14:21:49 -0500

    Add files via upload

--- page-creator-roadmap.php
+++ page-creator-roadmap.php
@@ -3,102 +3,23 @@
 ?><main>
     <div class="banner"><h1 class="centered-text banner-heading-38">Creator Roadmap</h1></div>
     <br>
-    <span class="purple-span" id="sellBooksSpan">How to Sell Books</span>
-    <div id="sellBooksSection" class="hidden">
-        <div class="centered-90vw">
-            <div class="left-gentle-bracket-orange">
-                <h2>Step 1. Add product(s) in the creator dashboard</h2>
-                <ul>
-                    <li>If you're selling the same book in multiple formats (such as e-book, audiobook, and paperback) add a separate product for each format.</li>
-                </ul>
-                <div class="tomc-map--blue-section">
-                    <p>Before you can add any products, you must choose how you'd like to be paid.</p>
-                    <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-billing-info.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlighting the Store Settings --Billing menu item." />
-                </div>
-                <div class="tomc-map--orange-section">
-                    <p>You can now navigate to Product Manager --Add Product in the side menu.</p>
-                    <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlighting the Product Manager --Add Product menu item." />
-                </div>
-                <div class="tomc-map--yellow-section">
-                    <p>Add your book's title and cover.</p>
-                    <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-title.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Product Title field." />
-                    <br>
-                    <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-image.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Upload Image field." />
-                </div>
-                <div class="tomc-map--purple-section">
-                    <p>If you already have an ISBN, you can add it now. You can also obtain a free ISBN by purchasing our ISBN registration service after you've added your product.</p>
-                    <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/gtin.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the GTIN field." />
-                </div>
-                <div class="tomc-map--blue-section">
-                    <p>Add your book's price. If you'd like to set a limited-time sale price, you can do that as well.</p>
-                    <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/price-info.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the pricing fields." />
-                </div>
-                <div class="tomc-map--orange-section">
-                    <p>Be sure to indicate which book format you're uploading.</p>
-                    <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-format.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the product format checkboxes." />
-                </div>
-                <p><strong>You'll need to complete other fields depending on the format you're uploading.</strong></p>
-                <span id="ebookProductsSpan" class="orange-button">E-book products</span>
-                <div id="ebookProductsSection" class="hidden">
-                    <p>Check the box next to Downloadable under Product Type.</p>
-                    <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/product-type.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the downloadable product type checkbox." />
-                    <p>Add your e-book file.</p>
-                    <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-file.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Add File button." />
-                    <br>
-                    <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-icon.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the upload icon." />
-                </div>
-                <span id="audiobookProductsSpan" class="blue-button">Audiobook products</span>
-                <div id="audiobookProductsSection" class="hidden">
-                    <p>Check the box next to Downloadable under Product Type.</p>
-                    <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/product-type.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the downloadable product type checkbox." />
-                    <p>Add your e-book file.</p>
-                    <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-file.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Add File button." />
-                    <br>
-                    <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-icon.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the upload icon." />
-                    <p>If you want to include a preview file, you can do so using the product gallery.</p>
-                    <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-gallery.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the product gallery field." />
-                </div>
-                <span id="physicalBookProductSpan" class="purple-button">Physical book products</span>
-                <div id="physicalBookProductSection" class="hidden">
-                    <p><strong>At this time, if you want to sell physical books, you must stock your own copies, manage your own inventory, and be responsible for shipping your own orders (including factoring US shipping into your pricing.)</strong></p>
-                    <p>Check the box by 'Enable stock management at product level.'</p>
-                    <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/enable_stock.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the 'Enable stock management at product level' checkbox." />
-                    <p>Supply values for the 'Stock quantity' and 'Low stock threshold' fields.</p>
-                    <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/stock.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the 'Stock quantity' and 'Low stock threshold' fields." />
-                    <p>The other Inventory fields are optional.</p>
+    <span class="roadmap--purple-span" id="sellBooksSpan">How to Sell Books</span>
+    <div id="sellProductsSection" class="roadmap-container">
+        <div class="roadmap-section">
+            <div class="roadmap-section--bottom-right--wrap-0">
+                    <div class="roadmap-section--bottom-right--wrap-1">
+                        <div class="roadmap-section--bottom-right--wrap-2">
+                            <div class="roadmap-section--bottom-right">
+                                <h2>Add Product From Creator Dashboard</h2>
+                                <p><strong>ADD PRODUCT FROM CREATORDASHBOARD</strong> Selling same book in multiple formats(e-book, audiobook, hardcopy)? Add a separate product for each format.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="centered-90vw">
-            <div class="left-gentle-bracket-purple">
-                <h2>Step 2. Add book info to help readers find your book</h2>
-            </div>
-        </div>
-    </div>
     <span class="blue-span" id="sellServicesSpan">How to Sell Services</span>
     <div id="sellServicesSection" class="hidden">
-        <div class="centered-90vw">
-            <div class="left-gentle-bracket-yellow">
-                <h2>Add your service as a product in the creator dashboard</h2>
-                <p>Before you can add any products, you must choose how you'd like to be paid.</p>
-                <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/add-billing-info.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlighting the Store Settings --Billing menu item." />
-                <p>You can now navigate to Product Manager --Add Product in the side menu.</p>
-                <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="a cropped screenshot of the Trunk of My Car creator dashboard highlighting the Product Manager --Add Product menu item." />
-                <p>Give your service a title and main image.</p>
-                <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/product-title.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Product Title field." />
-                <br>
-                <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/upload-image.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the Upload Image field." />
-                <p>If you want to add additional images, you can upload them with the Product Gallery field.</p>
-                <img class="orange-bordered-img" src="<?php echo get_theme_file_uri('/images/product-gallery.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the product gallery field." />
-                <p>Describe your service, including any instructions that you need buyers to follow.</p>
-                <img class="yellow-bordered-img" src="<?php echo get_theme_file_uri('/images/service-description.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the product gallery field." />
-                <p>Add a price for your service. If you'd like to set a limited-time sale price, you can do that as well.</p>
-                <img class="blue-bordered-img" src="<?php echo get_theme_file_uri('/images/price-info.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the pricing fields." />
-                <p>Check the box beside 'Services' in the Product Types section at the bottom of the page.</p>
-                <img class="purple-bordered-img" src="<?php echo get_theme_file_uri('/images/product-format.jpg'); ?>" alt="a cropped screenshot of the Add Product page in the Trunk of My Car creator dashboard highlighting the product format checkboxes." />
-                <br>
-            </div>
-        </div>
     </div>
 </main>
 
