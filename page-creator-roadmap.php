39dfe2407968ec32bd9d98a420a03ec55e02ffc5
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-12-18 18:41:36 -0500

    Add files via upload

--- page-creator-roadmap.php
+++ page-creator-roadmap.php
@@ -7,18 +7,31 @@
     <div id="sellProductsSection" class="roadmap-container">
         <div class="roadmap-section">
             <div class="roadmap-section--bottom-right--wrap-0">
-                    <div class="roadmap-section--bottom-right--wrap-1">
-                        <div class="roadmap-section--bottom-right--wrap-2">
-                            <div class="roadmap-section--bottom-right">
-                                <h2>Add Product From Creator Dashboard</h2>
-                                <p><strong>ADD PRODUCT FROM CREATORDASHBOARD</strong> Selling same book in multiple formats(e-book, audiobook, hardcopy)? Add a separate product for each format.</p>
-                            </div>
+                <div class="roadmap-section--bottom-right--wrap-1">
+                    <div class="roadmap-section--bottom-right--wrap-2">
+                        <div class="roadmap-section--bottom-right">
+                            <h2>Add Product From Creator Dashboard</h2>
+                            <p><strong>ADD PRODUCT FROM CREATOR DASHBOARD</strong> Selling the same book in multiple formats(e-book, audiobook, hardcopy)? Add a separate product for each format.</p>
                         </div>
                     </div>
                 </div>
             </div>
+            <img src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="test image" class="roadmap-img--bottom-left"/>
         </div>
-    <span class="blue-span" id="sellServicesSpan">How to Sell Services</span>
+        <div class="roadmap-section">
+            <div class="roadmap-section--bottom-left--wrap-0">
+                <div class="roadmap-section--bottom-left--wrap-1">
+                    <div class="roadmap-section--bottom-left--wrap-2">
+                        <div class="roadmap-section--bottom-left">
+                            <h2>Add Title and Cover</h2>
+                            <p>Already have an ISBN? Enter it here! Need one? TOMC has ISBNs available for a small registration fee here.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img src="<?php echo get_theme_file_uri('/images/add-file.jpg'); ?>" alt="test image" class="roadmap-img--bottom-right"/>
+        </div>
+    <span class="roadmap--blue-span" id="sellServicesSpan">How to Sell Services</span>
     <div id="sellServicesSection" class="hidden">
     </div>
 </main>
