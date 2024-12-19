9b5f939f676fd254d318d555110bef28d72a0d8f
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-12-19 12:41:32 -0500

    Add files via upload

--- page-creator-roadmap.php
+++ page-creator-roadmap.php
@@ -35,6 +35,71 @@
                 <img src="<?php echo get_theme_file_uri('/images/add-file.jpg'); ?>" alt="test image" />
             </div>
         </div>
+        <div class="roadmap-section">
+            <div class="roadmap-section--bottom-right--wrap-0">
+                <div class="roadmap-section--bottom-right--wrap-1">
+                    <div class="roadmap-section--bottom-right--wrap-2">
+                        <div class="roadmap-section--bottom-right">
+                            <h2>Upload Digital File</h2>
+                            <p>E-book or audiobook format? Upload your file <a href="<?php echo esc_url(site_url('/dashboard/edit-product'));?>" target="_blank">here</a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="roadmap-img--bottom-left blue-rounded-rhombus">
+                <img src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="test image"/>
+            </div>
+        </div>
+        <div class="roadmap-section">
+            <div class="roadmap-section--bottom-left--wrap-0">
+                <div class="roadmap-section--bottom-left--wrap-1">
+                    <div class="roadmap-section--bottom-left--wrap-2">
+                        <div class="roadmap-section--bottom-left">
+                            <h2 class="right-text-no-margin">Name Your Price</h2>
+                            <p class="right-text-no-margin">Set your book’s regular price and/or schedule a sale.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="roadmap-img--bottom-right purple-rounded-rhombus">
+                <img src="<?php echo get_theme_file_uri('/images/add-file.jpg'); ?>" alt="test image" />
+            </div>
+        </div>
+        <div class="roadmap-section">
+            <div class="roadmap-section--bottom-right--wrap-0">
+                <div class="roadmap-section--bottom-right--wrap-1">
+                    <div class="roadmap-section--bottom-right--wrap-2">
+                        <div class="roadmap-section--bottom-right">
+                            <h2>Select Book Format</h2>
+                            <p>One per product. Multiple formats require multiple products.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="roadmap-img--bottom-left blue-rounded-rhombus">
+                <img src="<?php echo get_theme_file_uri('/images/add-product.jpg'); ?>" alt="test image"/>
+            </div>
+        </div>
+        <div class="roadmap-section">
+            <div class="roadmap-section--bottom-left--wrap-0">
+                <div class="roadmap-section--bottom-left--wrap-1">
+                    <div class="roadmap-section--bottom-left--wrap-2">
+                        <div class="roadmap-section--bottom-left">
+                            <h2 class="right-text-no-margin">Choose How You Will Be Paid</h2>
+                            <p class="right-text-no-margin">Select 'Billing' under Store Settings on the Creator Dashboard and add your secure payment information.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="roadmap-img--bottom-right purple-rounded-rhombus">
+                <img src="<?php echo get_theme_file_uri('/images/add-file.jpg'); ?>" alt="test image" />
+            </div>
+        </div>
+        <div class="roadmap-section--final">
+
+        </div>
+        <br>
+    </div>
     <span class="roadmap--blue-span" id="sellServicesSpan">How to Sell Services</span>
     <div id="sellServicesSection" class="hidden">
     </div>
