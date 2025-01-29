8e2e90c22f54e7b33a3bfa26502377fd06b83947
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-01-29 09:20:02 -0500

    Add files via upload

--- page-sell-your-creations.php
+++ page-sell-your-creations.php
@@ -1,9 +1,13 @@
 <?php get_header();
 ?><main>
     <div class="two-thirds-screen">
-        <div class="banner"><h1 class="centered-text banner-heading-40">Sell Your Work</h1></div>
-        <div class="padding-x-20">
-            <p>Are you an author? A creative individual with services to offer? We invite you to sell your work on our platform whether you're interested in joining the cooperative or not (but we hope you'll consider joining!) Learn about the differences between selling as a creator-member and selling as a vendor below.</p>
+        <div class="banner"><h1 class="centered-text banner-heading-30">Options to Offer Your Work</h1></div>
+        <div>
+            <div class="padding-x-60 padding-t-20 flex-text">
+                <span>Are you an author? A creative individual with services to offer? We invite you to offer your work(s) through Trunk of My Car Cooperative,</span>
+                <span> whether you're interested in joining the cooperative or not (but we hope you'll consider joining!)</span>
+            </div>
+            <p class="centered-text padding-x-60">Learn about the differences between operating as a creator-member and as a vendor below.</p>
             <div class="fit-content margin-auto-block">
                 <table class="tomc-blue-purple-table">
                     <tr>
@@ -15,15 +19,15 @@
                         <td class="tomc-lighter-cells">Earn 80% royalties on book sales*</td>
                     </tr>
                     <tr>
-                        <td>Services offered have a 2.5% fee*</td>
-                        <td>Services offered have a 5% fee*</td>
+                        <td>2.5% fee on Service Offerings*</td>
+                        <td>5% fee on Service Offerings*</td>
                     </tr>
                     <tr>
                         <td class="tomc-lighter-cells">Eligible for year-end patronage, when available</td>
                         <td class="tomc-lighter-cells">-</td>
                     </tr>
                     <tr>
-                        <td>Enjoy a discount on products and services</td>
+                        <td>Enjoy discounts on products and services</td>
                         <td>-</td>
                     </tr>
                     <tr>
