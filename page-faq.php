b86fce6779f0091e1cc1d491c427325a1076c562
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-02-22 12:07:57 -0500

    Add files via upload

--- page-faq.php
+++ page-faq.php
@@ -2,9 +2,96 @@
 ?><main>
     <div class="two-thirds-screen">
         <div class="banner"><h1 class="centered-text banner-heading-54">FAQs</h1></div>
-        <?php wp_reset_postdata();
-        the_content(); ?>
-    ?></div>
+        <div class="max-1000-screen">
+            <div class="tomc-faq-section">
+                <h2>Why a Cooperative?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>We’re anti-capitalist, anti-resource hoarding, and idealists. The current self-publishing model is extractive and oppressive to both readers and writers. We exist to change that. We see indie authors out there doing all the things on their own and think there’s a better way – together. We envision writing community that is supportive, generative, generous, resourceful, and whole. Many hands make light work.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>Multi-Stakeholder?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>Everyone who interacts with our platform has the opportunity to co-own it. One or a few voices shouldn’t dictate for the many. We operate like a forest.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>Trunk of what, now?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>The name is a nod to pre-internet indie authors who used to print their books and sell them from the trunks of their cars outside grocery stores and malls. We are because they did.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>How does it work?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>Three circles of membership: Creators, Readers & Workers. Creators offer both books and services. Readers purchase books. Workers keep us on the road.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>What is a Creator?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>Authors, Editors, Cover Art Designers, Illustrators, Marketers, Presses. Creators have access to our sell-side marketplace.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>What’s in it for me?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>Readers receive a 15% discount. On everything. Immediately. Creators retain 90% of royalties. On everything. Immediately. And also 15% discount on books purchased.</p>
+                    <p>Both circles gain access to each other – beta and ARC readers; book clubs; Offers & Needs; other Creators; other Readers.</p>
+                    <p>All circles are eligible to participate in the governance of Trunk of My Car after 3 months of continuous paid membership dues.</p>
+                    <p>And profit-sharing. All circles are eligible for profit-sharing based on their participation on the platform.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>How much is the buy-in?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>We know that large buy-ins often exclude people from participating in cooperatives who are already otherwise excluded from marketplaces. We don’t do that here. We want to be as open and as inclusive as possible, while also keeping the tank full. We’ve established monthly dues for each Circle (time participation for Workers) that we think are fair and reasonable and enough for us to run on.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>Do I have to be a Member?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>No. We also understand that ownership isn’t for everybody. Authors can still register and sell on our site – royalties are 80% for non-members. We also offer ISBN registration at a cost of $15 per ISBN for non-members. Non-member readers do not receive discounts on purchases.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>What kind of books can I sell/buy?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>Right now, we are equipped to sell ebooks and audiobooks. As opportunities present, we will work with small, sustainable presses to print books both on demand and in bulk to provide such services for registered users. Creators are more than welcome to offer physical books/cds/cassettes for sale on the TOMC platform; however, physical works are the responsibility of the creator - that means printing, inventory and shipping are the creator's responsibility.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>When are dues due?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>Member-owner dues are invoiced every 30 days from the date the member first joined. If you register on the 15th, you’ll receive an invoice a few days in advance. Member dues not paid within 10 days of the due date will prompt account suspension. Such accounts will still have access to their bookshelves, but benefits will not accrue and voting rights shall be suspended until the account is brought current. Accounts not brought current within 3 billing cycles will be deemed closed. See your membership agreement for account reinstatement.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>Y’all sell ISBNs?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>No. We do not sell ISBNs. We do have ISBNs available for registered users on the platform and we do charge a small administrative fee for registering said ISBNs. Members pay $5 per ISBN registration and non-members pay $15. You do not need to have an ISBN to sell your books on our platform. Here are a few reasons why you might want one. Note, ISBNs received through the platform will list Trunk of My Car Cooperative as the publisher. And yes, you can use TOMC registered ISBNs on other platforms. And you can only use one ISBN per each type of book, so if you have an ebook and an audiobook, each version will require its own ISBN.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>Should I copyright my book?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>Copyright belongs to the person creating the art when the art is completed. But. You must register your completed work with the US Copyright Office if you’d like to protect your work. In other words, if someone steals your work, you cannot fight them unless your work is registered. Member resources include a copyright how-to and also a fill-in-the-blank DMCA takedown notice that can be sent in the event your work is stolen. While we do not offer DRM presently, authors can register with <a target="_blank" href="https://www.muso.com/antipiracy-protection-for-indie-creators">MUSO</a>. We also ask that you report any copyright violations to <a href="mailto:info@trunkofmycar.org">info@trunkofmycar.org</a> so that we can determine whether said violations are from our site. Copyright violators are banned indefinitely from the platform.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>What’s your stance on banned books?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>We don’t outright ban books. But, our membership agreement does prohibit the posting of work that contains directed hate speech or the glorification of abuse of any kind – members shall vote on any potential violations of this policy. We do understand, however, that difficult themes exist in works that aren’t intended to cause harm to others – we ask authors to tag such works with trigger warnings – an important step in creating safety in community.</p>
+                </div>
+            </div>
+            <div class="tomc-faq-section">
+                <h2>What about using AI?</h2>
+                <div class="tomc-faq-a hidden">
+                    <p>Works created, generated, or compiled by using Artificial Intelligence of any kind are banned from sale on the platform. Users found in violation of this policy shall be banned from the site.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
