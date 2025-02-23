18339e81b7705596347e57d7968ce035b3a41825
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-02-23 11:07:11 -0500

    Add files via upload

--- page-creator-resources.php
+++ page-creator-resources.php
@@ -4,7 +4,12 @@
     <div class="banner semi-large-heading"><h1 class="centered-text banner-heading-36">Creator Resources</h1></div>
     <br>
     <br>
-    <div class="generic-wrapper-2">
+    <?php if (is_user_logged_in()){
+        $user = wp_get_current_user();
+        if (in_array( 'creator-member', (array) $user->roles ) ||  in_array( 'administrator', (array) $user->roles )){
+            ?><br><br>
+            <div class="white-background-wrapper">
+            <div class="generic-wrapper-2">
         <div class="generic-wrapper-1">
             <div class="generic-wrapper-0">
                 <p class="centered-text">
@@ -14,7 +19,7 @@
                     <a href="<?php echo esc_url(site_url('/copyright-primer'));?>">Copyright Primer</a>
                 </p>
                 <p class="centered-text">
-                    <a href="<?php echo esc_url(site_url('/dmca-takedown-primer'));?>">DMCA Takedown Primer</a>
+                    <a href="<?php echo esc_url(site_url('/dmca-primer'));?>">DMCA Primer</a>
                 </p>
                 <p class="centered-text">
                     <a href="<?php echo esc_url(site_url('/dmca-takedown-template'));?>">DMCA Takedown Template</a>
@@ -22,7 +27,12 @@
             </div>
         </div>
     </div>
-</main>
+            </div>
+        <?php } else {
+            ?><p class="centered-text">To view the Creator Resources, join our Cooperative as a <a href="<?php echo esc_url(site_url('/creators-circle-membership'));?>">Creator-Member</a>.</p>
+        <?php }
+    }
+?></main>
 
 <?php get_footer();
 ?>
