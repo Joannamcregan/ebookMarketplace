aa284a35434ed633740d1e76e718a0f6b06659a5
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-02-23 11:09:29 -0500

    Add files via upload

--- page-creator-resources.php
+++ page-creator-resources.php
@@ -8,25 +8,23 @@
         $user = wp_get_current_user();
         if (in_array( 'creator-member', (array) $user->roles ) ||  in_array( 'administrator', (array) $user->roles )){
             ?><br><br>
-            <div class="white-background-wrapper">
             <div class="generic-wrapper-2">
-        <div class="generic-wrapper-1">
-            <div class="generic-wrapper-0">
-                <p class="centered-text">
-                    <a href="<?php echo esc_url(site_url('/creator-roadmap'));?>">Creator Roadmap</a>
-                </p>
-                <p class="centered-text">
-                    <a href="<?php echo esc_url(site_url('/copyright-primer'));?>">Copyright Primer</a>
-                </p>
-                <p class="centered-text">
-                    <a href="<?php echo esc_url(site_url('/dmca-primer'));?>">DMCA Primer</a>
-                </p>
-                <p class="centered-text">
-                    <a href="<?php echo esc_url(site_url('/dmca-takedown-template'));?>">DMCA Takedown Template</a>
-                </p>
-            </div>
-        </div>
-    </div>
+                <div class="generic-wrapper-1">
+                    <div class="generic-wrapper-0">
+                        <p class="centered-text">
+                            <a href="<?php echo esc_url(site_url('/creator-roadmap'));?>">Creator Roadmap</a>
+                        </p>
+                        <p class="centered-text">
+                            <a href="<?php echo esc_url(site_url('/copyright-primer'));?>">Copyright Primer</a>
+                        </p>
+                        <p class="centered-text">
+                            <a href="<?php echo esc_url(site_url('/dmca-primer'));?>">DMCA Primer</a>
+                        </p>
+                        <p class="centered-text">
+                            <a href="<?php echo esc_url(site_url('/dmca-takedown-template'));?>">DMCA Takedown Template</a>
+                        </p>
+                    </div>
+                </div>
             </div>
         <?php } else {
             ?><p class="centered-text">To view the Creator Resources, join our Cooperative as a <a href="<?php echo esc_url(site_url('/creators-circle-membership'));?>">Creator-Member</a>.</p>
