35a81d944cbdf4a1d85e3c4a1d533c148cfdf642
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-03-18 16:29:41 -0400

    Add files via upload

--- woocommerce/myaccount/payment-methods.php
+++ woocommerce/myaccount/payment-methods.php
@@ -21,7 +21,6 @@ defined( 'ABSPATH' ) || exit;
 
 ?><main>
     <div class="two-thirds-screen">
-        <div class="banner"><h1 class="centered-text banner-heading-54">404</h1></div>
         <h1 class="center-left-text">Sorry, looks like we took a wrong turn somewhere.</h1>
     </div>
 </main>
