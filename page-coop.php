9ec27117bac2d248adef1b421c824a7b1e5ea7c2
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-03-25 10:05:03 -0400

    Add files via upload

--- page-coop.php
+++ page-coop.php
@@ -1,7 +1,7 @@
 <?php get_header();
 
 ?><main class="leaves">
-    <h1 class='caprasimo-text special-display-text'>Trunk of My Car</h1>
+    <h1 class='caprasimo-text special-display-text'>Trunk of My Car Cooperative</h1>
     <p class="leaves-p centered-text"><strong>A Cooperatively Owned Self-Publishing Marketplace</strong></p>
     <div class="leaf-section">
         <div class="leaf-wrapper">
