cbfda4b57a501406ae339a523ff9c48c6513b49c
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-03-27 12:28:37 -0400

    Add files via upload

--- page-own.php
+++ page-own.php
@@ -5,7 +5,7 @@
         <div class="tomc-circle-container">
             <img src="<?php echo get_theme_file_uri('/images/circle-background-0.jpg'); ?>" role="presentation" />
             <p class="tomc-circle-p-0">This circle is for all our creatives out there. Those who write, design cover art, edit all the words, and/or are indie presses.</p>
-            <p class="orange-shadow-heading tankindred-text tomc-circle-p-1-padded-x"><a href="<?php echo esc_url(site_url('/creators-circle-membership'));?>"><strong>Creator</strong></a></p>
+            <p class="orange-shadow-heading tankindred-text tomc-circle-p-1-padded-2x"><a href="<?php echo esc_url(site_url('/creators-circle-membership'));?>"><strong>Creator</strong></a></p>
             <br><br>
             <p class="tomc-circle-p-2">Those who want to upload their work, collaborate with other members, and be in community, while reaping the benefits of ownership.</p>
         </div>
@@ -21,7 +21,7 @@
         <div class="tomc-circle-container tomc-circle-container-long-text">
             <img src="<?php echo get_theme_file_uri('/images/circle-background-2.jpg'); ?>" role="presentation" />
             <p class="tomc-circle-p-0">This circle is for those who run all the things behind the scenes, keeping Trunk of My Car on the road.</p>
-            <p class="orange-shadow-heading tankindred-text tomc-circle-p-1-padded-x"><strong>Worker</strong></p>
+            <p class="orange-shadow-heading tankindred-text tomc-circle-p-1-padded-2x"><strong>Worker</strong></p>
             <br><br>
             <p class="tomc-circle-p-2">Those who fill the tank, clean the windshield, and change the occasional tire. We don't have any job listings at the moment, but check back as we grow!</p>
         </div>
