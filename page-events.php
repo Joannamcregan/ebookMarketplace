173268f45cb2d8b3e60edbb0c017eda90aeda53a
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-04-16 17:05:46 -0400

    Add files via upload

---
+++ page-events.php
@@ -0,0 +1,17 @@
+<?php get_header();
+
+?><main>
+    <div class="banner semi-large-heading"><h1 class="centered-text banner-heading-36">Events</h1></div>
+    <br>
+    <br>
+    <div class="generic-wrapper-2">
+        <div class="generic-wrapper-1">
+            <div class="generic-wrapper-0">
+                <p class="centered-text">Coming soon!</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer();
+?>
