27f3f805dac5993e05032fd65395b9cce61ad67d
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-04-23 08:56:29 -0400

    Add files via upload

---
+++ page-hop-in.php
@@ -0,0 +1,12 @@
+<?php get_header();
+
+?><main>
+    <div class="banner"><h1 class="centered-text banner-heading-32">Hop In!</h1></div>
+    <br>
+    <br>
+    <div class="padding-x-20 half-screen">
+        <?php echo do_shortcode('[forminator_form id="5570"]'); ?>
+    </div>
+</main>
+
+<?php get_footer(); ?>
