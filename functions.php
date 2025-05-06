457ebccc8c27f156835e0c18e93a0661193b6f65
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-05-06 00:35:24 -0400

    Add files via upload

--- functions.php
+++ functions.php
@@ -697,11 +697,11 @@ function customer_email_recipient_change( $recipient_email, $order ) {
     }
 }
 //featured image screenshot-------------------------------------------------------------------------------------------
+add_theme_support( 'post-thumbnails' );
 function tomc_featured_image( $html ) {
     if ( '' == $html ) {
         return '<img src="' . get_template_directory_uri() . '/images/screenshot.png" width="150px" height="100px" class="image-size-name" />';
     }
-    // Else, return the post thumbnail
     return $html;
 }
 add_filter( 'post_thumbnail_html', 'tomc_featured_image' );
