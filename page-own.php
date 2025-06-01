ebf5295180fd53bb4636ed2389cd6446c5f77b88
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-06-01 12:24:26 -0400

    Add files via upload

--- page-own.php
+++ page-own.php
@@ -3,7 +3,10 @@
     <div class="two-thirds-screen">
         <div class="banner"><h1 class="centered-text banner-heading-40">Become a Member</h1></div>
         <div class="tomc-circle-container">
-            <img src="<?php echo get_theme_file_uri('/images/circle-background-0.jpg'); ?>" role="presentation" />
+            <picture>
+                <source type="image/webp" srcset="<?php echo get_theme_file_uri('/images/circle-background-0.webp'); ?>" role="presentation">
+                <img src="<?php echo get_theme_file_uri('/images/circle-background-0.jpg'); ?>" role="presentation" />
+            </picture>
             <p class="tomc-circle-p-0">This circle is for all our creatives out there. Those who write, design cover art, edit all the words, and/or are indie presses.</p>
             <p class="orange-shadow-heading tankindred-text tomc-circle-p-1-padded-2x"><a href="<?php echo esc_url(site_url('/creators-circle-membership'));?>"><strong>Creator</strong></a></p>
             <br><br>
@@ -11,7 +14,10 @@
         </div>
         <div class="red-orange-line-break-60"></div>
         <div class="tomc-circle-container">
-            <img src="<?php echo get_theme_file_uri('/images/circle-background-1.jpg'); ?>" role="presentation" />
+            <picture>
+                <source type="image/webp" srcset="<?php echo get_theme_file_uri('/images/circle-background-1.webp'); ?>" role="presentation">
+                <img src="<?php echo get_theme_file_uri('/images/circle-background-1.jpg'); ?>" role="presentation" />
+            </picture>
             <p class="tomc-circle-p-0">This circle is for those among us who love books just as much as creators but would rather read than write. </p>
             <p class="orange-shadow-heading tankindred-text tomc-circle-p-1-padded-2x"><a href="<?php echo esc_url(site_url('/readers-circle-membership'));?>"><strong>Reader</strong></a></p>
             <br><br>
@@ -19,7 +25,10 @@
         </div>
         <div class="red-orange-line-break-60"></div>
         <div class="tomc-circle-container tomc-circle-container-long-text">
-            <img src="<?php echo get_theme_file_uri('/images/circle-background-2.jpg'); ?>" role="presentation" />
+            <picture>
+                <source type="image/webp" srcset="<?php echo get_theme_file_uri('/images/circle-background-2.webp'); ?>" role="presentation">
+                <img src="<?php echo get_theme_file_uri('/images/circle-background-2.jpg'); ?>" role="presentation" />
+            </picture>
             <p class="tomc-circle-p-0">This circle is for those who run all the things behind the scenes, keeping Trunk of My Car on the road.</p>
             <p class="orange-shadow-heading tankindred-text tomc-circle-p-1-padded-2x"><strong>Worker</strong></p>
             <br><br>
