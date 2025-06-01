006124a7f0ed335c552b4e6e88cba1b16e5a428b
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-05-31 23:59:11 -0400

    Add files via upload

--- page-about.php
+++ page-about.php
@@ -20,11 +20,21 @@
         
         <div class="red-orange-line-break"></div>
         <h2 class="large-heading red-brown-text yellow-shadow-heading">Our Team</h2>
-        <img class="circle-bio-pic" alt="Black woman expressing joy" src="<?php echo get_theme_file_uri('/images/Zia.jpg'); ?>" />
+        <div class="circle-bio-pic-wrapper">
+            <picture>
+                <source type="image/webp" srcset="<?php echo get_theme_file_uri('/images/Zia.webp'); ?>" alt="Black woman expressing joy"  class="circle-bio-pic" >
+                <img alt="Black woman expressing joy" src="<?php echo get_theme_file_uri('/images/Zia.jpg'); ?>"  class="circle-bio-pic" />
+            </picture>
+        </div>
         <h3 class="centered-text red-brown-text">Zia M Dione</h3>
         <p>Founder. Operations Lead.</p>
         <p>Trunk of My Car Cooperative is inspired by a conversation Zia had with a famous author who shared how he started selling books from the trunk of his car. An emerging writer, she is focused on healing intergenerational trauma & building/supporting solidarity & sustainable economies. Zia is certified in permaculture design, has a law degree from the University of Baltimore & is currently pursuing an MFA in Creative Nonfiction at Pacific University. Yes, she changed her name.</p>
-        <img class="circle-bio-pic" alt="White woman with glasses stands in an art museum" src="<?php echo get_theme_file_uri('/images/joanna.jpg'); ?>" />
+        <div class="circle-bio-pic-wrapper">
+            <picture>
+                <source type="image/webp" srcset="<?php echo get_theme_file_uri('/images/joanna.webp'); ?>" alt="White woman with glasses stands in an art museum" class="circle-bio-pic">
+                <img alt="White woman with glasses stands in an art museum" src="<?php echo get_theme_file_uri('/images/joanna.jpg'); ?>" class="circle-bio-pic" />
+            </picture>
+        </div>
         <h3 class="centered-text red-brown-text">Joanna Cregan</h3>
         <p>Founding Member. Developer.</p>
         <p>Joanna is a reader, writer, and web developer from Northeast Ohio who believes cooperative ownership can help bring about a healthier world.</p>
