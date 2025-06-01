7c3c747ca4fc009bebd76722caf0309d334fe31b
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-06-01 11:41:59 -0400

    Add files via upload

--- page-discussions.php
+++ page-discussions.php
@@ -4,21 +4,27 @@
         <div class="banner"><h1 class="centered-text banner-heading-36">Spaces</h1></div>
         <?php if (is_user_logged_in()){
             $user = wp_get_current_user();
-            if ((in_array( 'reader-member', (array) $user->roles )) || (in_array( 'creator-member', (array) $user->roles ))){
+            if ((in_array( 'reader-member', (array) $user->roles )) || (in_array( 'creator-member', (array) $user->roles )) || (in_array( 'administrator', (array) $user->roles ))){
                 ?><div class="tomc-circle-container">
-                    <img src="<?php echo get_theme_file_uri('/images/circle-background-0.jpg'); ?>" role="presentation" />
+                    <picture>
+                        <source type="image/webp" srcset="<?php echo get_theme_file_uri('/images/circle-background-0.webp'); ?>" role="presentation">
+                        <img src="<?php echo get_theme_file_uri('/images/circle-background-0.jpg'); ?>" role="presentation" />
+                    </picture>
                     <p class="orange-shadow-heading tankindred-text tomc-circle-p-1"><a href="<?php echo esc_url(site_url('/forums/forum/members-only-space/'));?>"><strong>Members-Only</strong></a></p>
                     <p class="tomc-circle-p-2">A Space to Discuss All Things Cooperative</p>
                 </div>
                 <div class="red-orange-line-break-60"></div>
             <?php }
             ?><div class="tomc-circle-container tomc-circle-container-long-text">
-                <img src="<?php echo get_theme_file_uri('/images/circle-background-1.jpg'); ?>" role="presentation" />
+                <picture>
+                    <source type="image/webp" srcset="<?php echo get_theme_file_uri('/images/circle-background-1.webp'); ?>" role="presentation">
+                    <img src="<?php echo get_theme_file_uri('/images/circle-background-1.jpg'); ?>" role="presentation" />
+                </picture>
                 <p class="orange-shadow-heading tankindred-text tomc-circle-p-1-padded-2x"><a href="<?php echo esc_url(site_url('/forums/forum/general/'));?>"><strong>Public</strong></a></p>
                 <p class="tomc-circle-p-2">A Space to Discuss All Things Literary</p>
             </div>
         <?php } else {
-            ?>To participate in discussions, please <a href="<?php echo esc_url(site_url('/my-account'));?>">login</a>
+            ?><p class="padding-x-20 centered-text">To participate in discussions, please <a href="<?php echo esc_url(site_url('/my-account'));?>">login</a></p>
         <?php }
     ?></div>
 </main>
