84e93792f4ef619c1fe88bb9f6b43a3c75505d78
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-06-09 07:52:03 -0400

    Add files via upload

--- functions.php
+++ functions.php
@@ -722,3 +722,12 @@ function fb_opengraph() {
     }
 }
 add_action('wp_head', 'fb_opengraph', 5);
+//add support for title tag----------------------------------------------------------------------------
+if ( ! function_exists( '_wp_render_title_tag' ) ) {
+	function theme_slug_render_title() {
+?>
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<?php
+	}
+	add_action( 'wp_head', 'theme_slug_render_title' );
+}
