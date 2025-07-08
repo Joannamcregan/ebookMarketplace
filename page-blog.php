7b38b1f116d0082b8099de1a6d510cf6effcf4cf
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-08 13:43:06 -0400

    Add files via upload

--- page-blog.php
+++ page-blog.php
@@ -4,7 +4,7 @@
     <div class="banner"><h1 class="centered-text">Blog Posts</h1></div>
     <br>
     <br>
-    <div class="two-thirds-screen generic-content">
+    <div class="third-screen generic-content">
     <?php $args = array(
         'post_type'=> 'post',
         'orderby'    => 'ID',
@@ -16,9 +16,9 @@
         if ( $result-> have_posts() ) : ?>
         <?php while ( $result->have_posts() ) : $result->the_post(); ?>
         <div class="genre-category subcategory padding-x-20">
-            <a href='<?php echo get_the_permalink(); ?>'>
-            <?php the_title(); ?>  
-            </a> 
+            <p><a href='<?php echo get_the_permalink(); ?>'><?php the_title(); ?></a></p>     
+            <p><?php echo 'posted by ' . get_the_author() . ' on ' . get_the_date(); ?></p>
+            <div class="orange-yellow-line-break"></div>
         </div>
         <?php endwhile; ?>
         <?php endif; wp_reset_postdata(); ?>
