b94d26a74d2214d191bd5412d90af678372869fa
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-10-16 09:59:27 -0400

    Add files via upload

---
+++ comments.php
@@ -0,0 +1,32 @@
+<?php
+
+// Get only the approved comments
+$args = array(
+	'status' => 'approve',
+);
+
+// The comment Query
+$comments_query = new WP_Comment_Query();
+$comments       = $comments_query->query( $args );
+
+// Comment Loop
+if ( $comments ) {
+	foreach ( $comments as $comment ) {
+        echo '<div class="single-comment">';
+        echo '<h3>' . $comment->comment_author . '</h3>';
+        echo '<p>' . date('m', strtotime($comment->comment_date)) . '/' . date('d', strtotime($comment->comment_date)) . '/' . date('y', strtotime($comment->comment_date)) . '</p>';
+		echo '<p>' . $comment->comment_content . '</p>';
+        echo '</div>';
+	}
+} else {
+	echo 'No comments found.';
+}
+
+// Form
+comment_form([
+	'title_reply' => esc_html__( 'Leave a Reply', 'st-church' ),
+	'comment_field' => '<label for="comment">Comment</label><textarea name="comment" id="comment" required="required"></textarea>',
+	'label_submit' => esc_html__( 'Post Comment', 'st-church' )
+]);
+
+?>
--- single.php
+++ single.php
@@ -17,12 +17,28 @@
         </div>
     <?php }
     ?></div>
-    <div class="right-text by-line">
-        <a href="<?php echo esc_url(site_url('/blog'));?>">See all blog posts</a>
-    </div>
+    <?php global $wpdb;
+        $author_id = get_the_author_meta('ID');
+        $data_table = $wpdb->prefix . "bp_xprofile_data";
+        $fields_table = $wpdb->prefix . "bp_xprofile_fields";
+        $query = 'select data.value from %i data join %i fields
+            on data.field_id = fields.id
+            and data.user_id = %d
+            and fields.name = %s';
+        $results = $wpdb->get_results($wpdb->prepare($query, $data_table, $fields_table, $author_id, 'Newsletter Signup Link'), ARRAY_A);
+        if ($results){
+            ?><h2 class="centered-text padding-x-20"><a href="<?php echo $results[0]['value'] ?>" target="_blank">Signup for <?php echo get_the_author(); ?>'s newlsetter</a></h2>
+        <?}
+    ?><div class="blue-purple-line-break-60"></div>
     <div class="padding-x-20">
         <h2 class="centered-text">Comments</h2>
-        <?php get_template_part('template-parts/comments'); ?>
+        <!-- <?php get_template_part('template-parts/comments'); ?> -->
+         <?php if ( comments_open() || get_comments_number() ) :
+            comments_template();
+        endif; ?>
+    </div>
+    <div class="right-text by-line">
+        <a href="<?php echo esc_url(site_url('/blog'));?>">See all blog posts</a>
     </div>
 </main>
 
