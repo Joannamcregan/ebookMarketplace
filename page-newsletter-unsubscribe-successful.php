88c480a3fa46c6e8c7ed7db493136df9ce8dbf7d
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-11-24 19:39:35 -0500

    Add files via upload

--- page-newsletter-unsubscribe-successful.php
+++ page-newsletter-unsubscribe-successful.php
@@ -1,12 +1,17 @@
 <?php get_header();
 
 ?><main>
-    <div class="banner"><h1 class="centered-text banner-heading-50">Unsubscribe</h1></div>
+    <div class="banner"><h1 class="centered-text banner-heading-50">Success</h1></div>
     <br>
     <br>
     <div class="padding-x-20 centered-text half-screen">
-        <?php echo do_shortcode('[mailpoet_page]'); ?>
-    </div>
+        <?php if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+                echo do_shortcode('[mailpoet_page]');
+                echo the_content();
+            endwhile;
+        endif;    
+    ?></div>
 </main>
 
 <?php get_footer();
