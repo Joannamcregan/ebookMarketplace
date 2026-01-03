acc15eaee17bdb27b39dfeba332e50062bd7ece7
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-01-03 07:07:46 -0500

    Add files via upload

--- functions.php
+++ functions.php
@@ -472,7 +472,7 @@ function assignReaderMemberRole($form_id, $response) {
 }
 
 function assignCreatorMemberRole($form_id, $response) {
-    if( $response['success']  && $form_id ==4212 /* Creator-Member Signup */){
+    if( $response['success']  && ($form_id ==4212 /* Creator-Member Signup */ || $form_id ==6297 /* creator-membership for organizations*/)){
         global $wpdb;
         $usermeta_table = $wpdb->prefix . "usermeta";
         $user = wp_get_current_user();
